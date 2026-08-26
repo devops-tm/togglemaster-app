471ba1f32e30a2280768739b94f7fd765dc28b49
cristian <cristian@192.168.1.11>
2026-08-26 17:52:37 -0300

    fix terraform namespace eks

--- terraform/environments/prod/compute/main.tf
+++ terraform/environments/prod/compute/main.tf
@@ -12,9 +12,49 @@ terraform {
   }
 }
 
+
+# ------------------------------------------------------------------
+# PROVIDERS
+# ------------------------------------------------------------------
+
+terraform {
+  backend "s3" {
+    bucket         = "togglemaster-terraform-state"
+    key            = "prod/compute/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 
 module "eks" {
   source = "../../../modules/eks"
