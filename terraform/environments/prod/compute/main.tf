90ed0db4505cd3f4f8bce423b24d4f0314455ee6
cristian <cristian@192.168.1.11>
2026-08-27 22:17:37 -0300

    fix terraform

--- terraform/environments/prod/compute/main.tf
+++ terraform/environments/prod/compute/main.tf
@@ -24,10 +24,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_name
-}
-
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
 }
