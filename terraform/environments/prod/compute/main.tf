fcb141fd1109dd15fb76616cb92f74c3389dbf8b
cristian <cristian@192.168.1.11>
2026-08-27 21:51:31 -0300

    fix terraform

--- terraform/environments/prod/compute/main.tf
+++ terraform/environments/prod/compute/main.tf
@@ -33,8 +33,8 @@ data "aws_eks_cluster_auth" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
